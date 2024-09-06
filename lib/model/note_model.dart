390f3c2ada9a26dbecb76faabf9fb75464c4e043
Muhanad-eng <dbo.muhanad@gmail.com>
2024-09-06 16:02:50 +0300

    add hive package

---
+++ lib/model/note_model.dart
@@ -0,0 +1,8 @@
+class NoteModel {
+ final String tite;
+ final String subtitle;
+ final String date;
+final int color;
+
+  NoteModel({required this.tite, required this.subtitle, required this.date, required this.color});
+}
